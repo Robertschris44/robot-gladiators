ef4bf2c8d42eb464e6b6dac6bd162e455cec5510
Chris Key <robertschris010@gmail.com>
2022-06-09 00:18:06 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
+//delete this when you see this
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
